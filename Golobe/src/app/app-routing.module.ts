0b3b732d27d25a5fa85a6c87aa9598d1f52cc31a
Aswini Babu <aswini.babu@speridian.com>
2023-06-16 23:32:26 +0530

    feat(Login): UI

--- Golobe/src/app/app-routing.module.ts
+++ Golobe/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'',
+    loadChildren : ()=>import('./pre-auth/pre-auth.module'). then (m=>m.PreAuthModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ Golobe/src/app/pre-auth/login/login.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss']
+})
+
+
+export class LoginComponent implements OnInit {
+  slides: any[] = new Array(3).fill({ id: -1, src: '', title: '', subtitle: '' });
+  constructor() { }
+
+  ngOnInit(): void {
+    this.slides[0] = {
+      src: './assets/sign-up.png',
+    };
+    this.slides[1] = {
+      src: './assets/sign-up.png',
+    }
+    this.slides[2] = {
+      src: './assets/sign-up.png',
+    }
+  }
+}
